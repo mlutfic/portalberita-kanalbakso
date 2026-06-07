d7f431960812c8597841f7888ca4eff99622c41b
Khoirul Arifin <khoirularifin091196@gmail.com>
2026-06-07 16:03:53 +0700

    configure supabase publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.KAB_SUPABASE_URL = "https://jzgxifbyndrmrwqvriuo.supabase.co";
-window.KAB_SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
+window.KAB_SUPABASE_ANON_KEY = "sb_publishable_Mi1AsYxtSpNdD0g4dgsL8A_ht15jXU7";
